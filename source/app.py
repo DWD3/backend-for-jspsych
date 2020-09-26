45b3373de139dd197ba67ef5cab33f4e3b644a4c
DWD3 <wjcauskew@hotmail.com>
2020-09-26 14:49:50 +1000

    added s3 to the resource list

--- source/app.py
+++ source/app.py
@@ -7,4 +7,5 @@ app = Chalice(app_name='backend-jspsych')
 @app.route('/uploadresult',methods=['POST'])
 def upload_result():
     domain_name = os.environ.get('SimpleDBDomainName', 'ExpResultSDB')
-    return {"result":domain_name}
+    bucket_name = os.environ.get('S3BucketName','')
+    return {"S3BucketName":domain_name,"bucket_name":bucket_name}
